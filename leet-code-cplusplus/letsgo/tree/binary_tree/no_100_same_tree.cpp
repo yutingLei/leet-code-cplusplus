493b3255f4337d21bff09bc5ec639b8dbae6b17a
lyt <1049373170@qq.com>
2022-05-16 11:29:12 +0800

    completed no. 100

---
+++ leet-code-cplusplus/letsgo/tree/binary_tree/no_100_same_tree.cpp
@@ -0,0 +1,20 @@
+//
+//  no_100_same_tree.cpp
+//  leet-code-cplusplus
+//
+//  Created by admin on 2022/5/16.
+//
+
+#include "no_100_same_tree.hpp"
+
+/** 本题相对简单
+ *
+ *  思路: 分别对比左右节点, 然后一次对比节点的节点.
+ */
+bool isSameTree(TreeNode* p, TreeNode* q) {
+    if (p == nullptr || q == nullptr)
+        return p == q;
+    if (p->val != q->val)
+        return false;
+    return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+}
