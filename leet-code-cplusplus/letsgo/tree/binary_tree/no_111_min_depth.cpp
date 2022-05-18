bc04fc9dc99f0c29faa0cc1ddd8a848769abcd7e
lyt <1049373170@qq.com>
2022-05-18 18:31:04 +0800

    add no. 111

---
+++ leet-code-cplusplus/letsgo/tree/binary_tree/no_111_min_depth.cpp
@@ -0,0 +1,45 @@
+//
+//  no_111_min_depth.cpp
+//  leet-code-cplusplus
+//
+//  Created by admin on 2022/5/18.
+//
+
+#include <queue>
+#include "no_111_min_depth.hpp"
+
+using namespace std;
+
+/**
+ * 本题考察的其实就是二叉树的遍历
+ *
+ * 解题思路:
+ *      求二叉树的最小深度, 可以理解为找到第一个叶节点, 叶节点是指没有左右儿子的节点.
+ *      求第一个没有页节点的左右儿子, 应该由自顶向下遍历.
+ *
+ */
+
+int minDepth(TreeNode* root) {
+    if (!root) { return 0; }
+    queue<TreeNode*> q;
+    q.push(root);
+    int ans = 1;
+    while (!q.empty()) {
+        size_t qsize = q.size();
+        for (int i = 0; i < qsize; i++) {
+            TreeNode* node = q.front();
+            q.pop();
+            if (node->left == nullptr || node->right == nullptr) {
+                return ans;
+            }
+            if (node->left) {
+                q.push(node->left);
+            }
+            if (node->right) {
+                q.push(node->right);
+            }
+        }
+        ans++;
+    }
+    return ans;
+}
---
+++ leet-code-cplusplus/letsgo/tree/binary_tree/no_111_min_depth.hpp
@@ -0,0 +1,22 @@
+//
+//  no_111_min_depth.hpp
+//  leet-code-cplusplus
+//
+//  Created by admin on 2022/5/18.
+//
+
+#ifndef no_111_min_depth_hpp
+#define no_111_min_depth_hpp
+
+#include <stdio.h>
+#include "tree_node.hpp"
+
+/**
+ * 题目111: 二叉树的最小深度
+ *
+ * 描述: 给定一个二叉树, 找出其最小深度.
+ *      最小深度是从根节点到最近叶子节点的最短路径上的节点数量.
+ *      说明: 叶子节点是指没有子节点的节点.
+ */
+int minDepth(TreeNode* root);
+#endif /* no_111_min_depth_hpp */
