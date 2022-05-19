ca6bfa6ad30736da2c356bcb5af132de19de268f
lyt <1049373170@qq.com>
2022-05-19 09:34:07 +0800

    completed no. 112

---
+++ leet-code-cplusplus/letsgo/tree/binary_tree/no_112_has_path_sum.cpp
@@ -0,0 +1,60 @@
+//
+//  no_112_has_path_sum.cpp
+//  leet-code-cplusplus
+//
+//  Created by admin on 2022/5/19.
+//
+
+#include <queue>
+#include "no_112_has_path_sum.hpp"
+
+using namespace std;
+
+/**
+ * 解题思路:
+ *      需要找到这么一条从根节点到叶节点路劲, 它们的和与给定的值相同.
+ *      分解一下就可以, 已知根节点的值, 那么只需要找到左或右子树是否
+ *      有一条路径的和为targetSum-root->val, 如果有, 那么加上根节点
+ *      的值就可以组成和为targetSum的路径.
+ *      同理, 左右子树路径和又可以分解为找它们左右子树的和.
+ */
+
+bool hasPathSum(TreeNode* root, int targetSum) {
+    /// 递归法
+//    // 1. 如果节点为空, 直接返回false
+//    if (!root) return false;
+//    // 2. 如果是叶节点, 判断叶节点的值是否与目标值相同
+//    if (!root->left && !root->right) {
+//        return targetSum == root->val;
+//    }
+//    // 3. 寻找该节点子树是否存在目标和
+//    return hasPathSum(root->left, targetSum - root->val) || hasPathSum(root->right, targetSum - root->val);
+    
+    /// 迭代法
+    if (!root) return false;
+    queue<TreeNode*> q;
+    queue<int> targets;
+    q.push(root);
+    targets.push(targetSum);
+    while (!q.empty()) {
+        size_t qsize = q.size();
+        for (int i = 0; i < qsize; i++) {
+            TreeNode* node = q.front();
+            q.pop();
+            int target = targets.front();
+            targets.pop();
+            if (!node->left && !node->right && node->val == target) {
+                return true;
+            }
+            if (node->left) {
+                q.push(node->left);
+                targets.push(target - node->val);
+            }
+            if (node->right) {
+                q.push(node->right);
+                targets.push(target - node->val);
+            }
+        }
+    }
+    return false;
+}
---
+++ leet-code-cplusplus/letsgo/tree/binary_tree/no_112_has_path_sum.hpp
@@ -0,0 +1,24 @@
+//
+//  no_112_has_path_sum.hpp
+//  leet-code-cplusplus
+//
+//  Created by admin on 2022/5/19.
+//
+
+#ifndef no_112_has_path_sum_hpp
+#define no_112_has_path_sum_hpp
+
+#include <stdio.h>
+#include "tree_node.hpp"
+
+/**
+ * 题目112: 路径总和
+ *
+ * 描述: 给你二叉树的根节点 root 和一个表示目标和的整数 targetSum.
+ * 判断该树中是否存在 根节点到叶子节点 的路径, 这条路径上所有节点值相加
+ * 等于目标和 targetSum. 如果存在, 返回 true; 否则, 返回 false.
+ *
+ * 注: 叶子节点 是指没有子节点的节点.
+ */
+bool hasPathSum(TreeNode* root, int targetSum);
+#endif /* no_112_has_path_sum_hpp */
